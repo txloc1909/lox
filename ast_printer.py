81a8a1dc0ec57bf848c4f5c593ee17ee09426211
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-06-27 00:22:54 +0700

    Initial `Expr` types, `Visitor` pattern and AST pretty printer

---
+++ ast_printer.py
@@ -0,0 +1,49 @@
+from scanner import TokenType, Token
+from expr import (
+    Visitor,
+    Expr,
+    Binary,
+    Grouping,
+    Literal,
+    Unary,
+)
+
+
+class ASTPrinter:
+
+    def print(self, expr: Expr):
+        return expr.accept(self)
+
+    def visit(self, expr: Expr):
+        return getattr(self, f"visit_{expr.__class__.__name__}")(expr)
+
+    def visit_Binary(self, expr: Binary):
+        return self._parenthesize(expr.operator.lexeme, expr.left, expr.right)
+
+    def visit_Grouping(self, expr: Grouping):
+        return self._parenthesize("group", expr.inner)
+
+    def visit_Literal(self, expr: Literal):
+        return "nil" if expr.value is None else str(expr.value)
+
+    def visit_Unary(self, expr: Unary):
+        return self._parenthesize(expr.operator.lexeme, expr.right)
+
+    def _parenthesize(self, name: str, *exprs: Expr):
+        return f"({name} " + " ".join(e.accept(self) for e in exprs) + ")"
+
+
+
+if __name__ == "__main__":
+    assert issubclass(ASTPrinter, Visitor)
+
+    test_expression = Binary(
+        left=Unary(
+            Token(TokenType.MINUS, "-", None, 1),
+            Literal(value=123),
+        ),
+        operator=Token(TokenType.STAR, "*", None, 1),
+        right=Grouping(inner=Literal(value=45.67)),
+    )
+
+    print(ASTPrinter().print(test_expression))
---
+++ expr.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import Protocol, runtime_checkable
+from numbers import Real
+
+from scanner import Token
+
+
+@runtime_checkable
+class Visitor(Protocol):
+    def visit(self, expr):
+        '''
+        Dynamically dispatching according to the type of `expr`
+        '''
+        pass
+
+
+class Expr:
+    def accept(self, visitor: Visitor):
+        return visitor.visit(self)
+
+
+@dataclass
+class Binary(Expr):
+    left: Expr
+    operator: Token
+    right: Expr
+
+
+@dataclass
+class Grouping(Expr):
+    inner: Expr
+
+
+@dataclass
+class Literal(Expr):
+    value: Real | str | None
+
+
+@dataclass
+class Unary(Expr):
+    operator: Token
+    right: Expr
