d1bacc96addea03725e5b642b4d92ed624a1f432
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-07-06 16:32:12 +0700

    class instantialization

--- _class.py
+++ _class.py
@@ -1,9 +1,25 @@
 from dataclasses import dataclass
 
+from callable import LoxCallable
+
 
 @dataclass
-class LoxClass:
+class LoxInstance:
+    _class: "LoxClass"
+
+    def __repr__(self):
+        return f"{self._class.name} instance"
+
+
+@dataclass
+class LoxClass(LoxCallable):
     name: str
 
     def __repr__(self):
         return self.name
+
+    def call(self, interpreter, *arguments):
+        return LoxInstance(_class=self)
+
+    def arity(self) -> int:
+        return 0
