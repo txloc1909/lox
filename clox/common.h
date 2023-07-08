1f19465a16719b71ea19fcc402c423a62600a653
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-07-08 23:49:50 +0700

    initial virtual machine and a simple negate op

--- clox/common.h
+++ clox/common.h
@@ -5,5 +5,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define DEBUG_TRACE_EXECUTION
+
 #endif // !clox_common_h
 
---
+++ clox/vm.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+#include "chunk.h"
+#include "common.h"
+#include "debug.h"
+#include "value.h"
+#include "vm.h"
+
+
+VM vm;
+
+static void resetStack() {
+    vm.stackTop = vm.stack;
+}
+
+
+void initVM() {
+    resetStack();
+}
+
+void freeVM() {
+
+}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    while (1) {
+
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif /* ifdef DEBUG_TRACE_EXECUTION */
+
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                push(constant);
+                break;
+            }
+            case OP_NEGATE: { push(-pop()); break; }
+            case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
+            }
+        }
+
+    }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
+
