f51bdd46a0e03a34824aa73b4b9e906e91b17115
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-07-10 00:25:49 +0700

    [clox] scanner

--- clox/vm.c
+++ clox/vm.c
@@ -2,6 +2,7 @@
 
 #include "chunk.h"
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "value.h"
 #include "vm.h"
@@ -77,9 +78,8 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
 
