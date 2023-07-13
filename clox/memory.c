5fa34b27be938a45c03154492d2ec58920a36551
Tran Xuan Loc <loc.tran@moreh.com.vn>
2023-07-13 15:20:54 +0700

    [clox] support string type, and some minimal memory management in VM

--- clox/memory.c
+++ clox/memory.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "memory.h"
+#include "vm.h"
 
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
@@ -12,5 +13,24 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
     void* result = realloc(pointer, newSize);
     if (result == NULL) exit(1);
     return result;
+}
+
+void freeObject(Obj* object) {
+    switch (object->type) {
+        case OBJ_STRING: {
+            ObjString* string = (ObjString*)object;
+            FREE_ARRAY(char, string->chars, string->length + 1);
+            FREE(ObjString, object);
+        }
+    }
+}
+
+void freeObjects() {
+    Obj* object = vm.objects;
+    while (object != NULL) {
+        Obj* next = object->next;
+        freeObject(object);
+        object = next;
+    }
 }
 
--- clox/value.c
+++ clox/value.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
+#include <string.h>
 
+#include "object.h"
 #include "memory.h"
 #include "value.h"
 
@@ -32,6 +34,7 @@ void printValue(Value value) {
             break;
         case VAL_NIL: printf("nil"); break;
         case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+        case VAL_OBJ: printObject(value); break;
     }
 
 }
@@ -43,6 +46,12 @@ bool valuesEqual(Value a, Value b) {
         case VAL_BOOL:      return AS_BOOL(a) == AS_BOOL(b);
         case VAL_NIL:       return true;
         case VAL_NUMBER:    return AS_NUMBER(a) == AS_NUMBER(b);
+        case VAL_OBJ: {
+            ObjString* aStr = AS_STRING(a);
+            ObjString* bStr = AS_STRING(b);
+            return (aStr->length == bStr->length) &&
+                memcmp(aStr->chars, bStr->chars, aStr->length) == 0;
+        }
         default:            return false;   // unreachable
     }
 }
