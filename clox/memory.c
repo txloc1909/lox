f0a613ff12788d953ac5cb452dd8a96100d823fa
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-07-20 00:09:27 +0700

    [clox] bound and call method on instance

--- clox/memory.c
+++ clox/memory.c
@@ -42,6 +42,10 @@ void freeObject(Obj* object) {
     printf("%p free type %d\n", (void*)object, object->type);
 #endif
     switch (object->type) {
+        case OBJ_BOUND_METHOD: {
+            FREE(ObjBoundMethod, object);
+            break;
+        }
         case OBJ_CLASS: {
             ObjClass* klass = (ObjClass*)object;
             freeTable(&klass->methods);
@@ -132,6 +136,12 @@ static void blackenObject(Obj* object) {
     printf("\n");
 #endif
     switch (object->type) {
+        case OBJ_BOUND_METHOD: {
+            ObjBoundMethod* bound = (ObjBoundMethod*)object;
+            markValue(bound->receiver);
+            markObject((Obj*)bound->method);
+            break;
+        }
         case OBJ_CLASS: {
             ObjClass* klass = (ObjClass*)object;
             markObject((Obj*)klass->name);
